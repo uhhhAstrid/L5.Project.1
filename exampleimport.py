cb63dfdc60c83ff48b435ccf1fdc9041794e140b
astrid <uhhhAstrid@gmail.com>
2025-12-02 18:35:57 -0500

    added more instructions

--- exampleimport.py
+++ exampleimport.py
@@ -1,3 +1,6 @@
+# This is how you import code from other files
 import teacher as teach
 
+# This is how you use code from other files.
+# In this case, we're calling a function.
 teach.everythingTogether()
