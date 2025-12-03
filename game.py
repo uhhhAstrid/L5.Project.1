90ea7276e5f0b31de2897bfe389cb381049ec8ab
John Doe <johndoe@email.com>
2025-12-02 18:41:18 -0800

    e

--- game.py
+++ game.py
@@ -1,6 +1,20 @@
 # this will be the main file for the game
+import random
+import pygame
+
+
 
 def game():
     print("Game started.")
 
+    class Player_1:
+        endzone = ["SafetyZoneP1", "HomeP1"]
+    class Player_2:
+        endzone = ["SafetyZoneP2", "HomeP2"]
+
 game()
+
+
+
+
+
