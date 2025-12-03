694e84a3c80fae35d8eb11369e364d673acc8192
lillyrose pember <lillyrose.pember@gmail.com>
2025-12-02 19:40:39 -0700

    added basic player turn

--- game.py
+++ game.py
@@ -1,6 +1,31 @@
-# this will be the main file for the game
+import random
+#import card info
+#import board setup
+
+def shuffle():
+    #recreate card_list based on card info
+    random.shuffle(card_list)
+
+def draw_card():
+    if card_list != []:
+        #adapt based on card info
+        card = random.choice(card_list)
+        #adapt based on card info
+        card_list.remove(card)
+    else:
+        shuffle()
+        draw_card()
+
+def act_card():
+    #adapt based on card info
+    card.effect
+
+def player_turn():
+    draw_card()
+    act_card()
+
 
 def game():
-    print("Game started.")
+    
 
 game()
