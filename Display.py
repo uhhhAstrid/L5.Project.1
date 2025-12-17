99d3b9388a5a08be1d38541ca1be4ba4836917de
John Doe <johndoe@email.com>
2025-12-16 17:44:59 -0800

    added display

---
+++ Display.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+window = Tk()
+window.title("Sorry")
+window.geometry("500x500")
+text_label_1 = Label(window, text = "Player One")
+text_label_2 = Label(window, text = "Player Two")
+text_label_1.place(x=20, y=0)
+text_label_2.place(x=400, y=0)
+text_label_cards = Label(window, text = ("draw"))
+
+
+def clicked():
+    Drawing_cards = Button( text = "Click me to draw a new card" ,fg = "red", command=clicked)
+
+window.mainloop()
