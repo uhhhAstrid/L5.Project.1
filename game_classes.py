8a73f554de848e9b04ace4735ee0910dca4949e0
lillyrose pember <lillyrose.pember@gmail.com>
2025-12-16 18:38:15 -0700

    added card to list

--- game_classes.py
+++ game_classes.py
@@ -1,59 +1,63 @@
 # this file will store all of the classes for the game
 # of course, this will also include all class functions and variables
 class Card():
+
     def __init__(self, number, effect):
         self.number = number
         self.effect = effect
+        self.card_list = []
+
 
     def card1classfunction(self):
         self.number = 1
         self.effect = move
+        self.card_list.append(self)
     
     def card2classfunction(self):
         self.number = -2
         self.effect = move
+        self.card_list.append(self)
+    
 
     def card3classfunction(self):
         self.number = -3
         self.effect = move
+        self.card_list.append(self)
     
     def card4classfunction(self):
         self.number = 4
         self.effect = move
+        self.card_list.append(self)
 
     def card5classfunction(self):
         self.number = 5
         self.effect = move
+        self.card_list.append(self)
 
     def card7classfunction(self):
         self.number = 7
         self.effect = move or splitbetweenpieces
+        self.card_list.append(self)
     
     def card8classfunction(self):
         self.number = 8
         self.effect = move
+        self.card_list.append(self)
     
     def card10classfunction(self):
         self.number = 10
         self.effect = move and movedifcard1spaceback
+        self.card_list.append(self)
     
     def card11classfunction(self):  
         self.number = 11
         self.effect = move
+        self.card_list.append(self)
     
     def card12classfunction(self):
         self.number = 12
         self.effect = move and skipturn 
-
-
-class boardspace():
-    SafeZone = False
-    tiletotal = 32
-    tilenum = 0
-    if tilenum == 32:
-        SafeZone = True
-
-
+        self.card_list.append(self)
 
 
 class Piece():
